8f22dee170890aee144c83d5f494a72baf8c52e2
Vedat Biner <vbiner@gmail.com>
2022-05-28 17:59:39 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_btk_anket/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_btk_anket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+// Firebase ile çalışma
+// sahte data ile çalışıp, listview oluşturmak gerekiyor.
+// ancak bir hata var bulamadım.
+// sonuç olarak bu kod hatalı.
+
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Anket"),
+        ),
+        body: const SurveyList(),
+      ),
+    );
+  }
+}
+
+class SurveyList extends StatefulWidget{
+  const SurveyList({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return SurveyListState();
+  }
+}
+
+class SurveyListState extends State{
+  @override
+  Widget build(BuildContext context) {
+    return buildBody(context, sahteSnapshot);
+  }
+
+  Widget buildBody(BuildContext context, List<Map<String, Object>> snapshot) {
+    return  ListView (
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map<Widget>((data) => buildListItem(context, data)).toList(),
+    );
+  }
+
+  buildListItem(BuildContext context, Map data) {
+    // final row = Anket.fromMap(data); // asıl kod
+    final row = Anket.fromMap(data as Map<String, dynamic>, reference: sahteSnapshot);
+    return Padding(
+      key: ValueKey(row.isim),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(5.0)
+          ),
+        child: ListTile(
+          title: Text(row.isim),
+          trailing: Text(row.oy.toString()),
+          onTap: () => print(row.isim),
+          ),
+        )
+      );
+  }
+
+}
+
+class Anket{
+  String isim;
+  int oy;
+  DocumentReference reference;
+
+  Anket.fromMap(Map<String, dynamic> map, {required this.reference})
+    : assert (map["isim"] != null), assert (map["oy"] != null),
+      isim = map["isim"], oy = map["oy"];
+
+  Anket.fromSnapshot(DocumentSnapshot snapshot)
+    : this.fromMap(snapshot.data as Map<String, dynamic>, reference:snapshot.reference);
+}
+
+// Sahte data map formatında gelecek
+final sahteSnapshot = [
+  {"isim":"C#", "oy":3},
+  {"isim":"Java", "oy":4},
+  {"isim":"Dart", "oy":5},
+  {"isim":"C++", "oy":7},
+  {"isim":"Python", "oy":90},
+  {"isim":"Perl", "oy":2},
+];
